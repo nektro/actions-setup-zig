9cb7704c306cb56b74d00b912aea936a1961d36e
Meghan Denny <hello@nektro.net>
2021-12-12 18:35:58 -0800

    implementation

--- index.js
+++ index.js
@@ -1,21 +1,68 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const os = require("os");
 
+const actions = require("@actions/core");
+const cache = require("@actions/tool-cache")
+
+const { Octokit } = require("@octokit/rest");
+const { throttling } = require("@octokit/plugin-throttling");
+
+const decompress = require("decompress");
+const decompressTarxz = require("decompress-tarxz");
+
+const OctokitImpl = Octokit.plugin(throttling);
+
+const octokit = new OctokitImpl({
+    throttle: {
+        onRateLimit: (retryAfter, options) => {
+            octokit.log.warn(
+                `Request quota exhausted for request ${options.method} ${options.url}`
+            );
+            // Retry twice after hitting a rate limit error, then give up
+            if (options.request.retryCount <= 2) {
+                console.log(`Retrying after ${retryAfter} seconds!`);
+                return true;
+            }
+        },
+        onAbuseLimit: (retryAfter, options) => {
+            // does not retry, only logs a warning
+            octokit.log.warn(
+                `Abuse detected for request ${options.method} ${options.url}`
+            );
+        },
+    },
+});
+
+const osMap = {
+    linux: "linux",
+    darwin: "macos",
+    win32: "windows",
+};
+
+const archMap = {
+    x64: "x86_64",
+    arm64: "aarch64",
+};
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const latest_version = await octokit.repos.listReleases({ owner: "ziglang", repo: "zig" }).then(x => x.data[0].tag_name);
+    const version = actions.getInput("version") || latest_version;
+    const master_build = version.includes("-dev.");
+    const folder = master_build ? "builds" : `download/${version}`;
+    const the_os = osMap[os.platform()];
+    const the_arch = archMap[os.arch()];
+    const ext = the_os === "windows" ? "zip" : "tar.xz";
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    const zig_folder = `zig-${the_os}-${the_arch}-${version}`;
+    const zig_file = `${zig_folder}.${ext}`;
+    const zig_url = `https://ziglang.org/${folder}/${zig_file}`;
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    return Promise.resolve(zig_url)
+        .then(x => cache.downloadTool(x))
+        .then(_ => decompress(zig_file, zig_folder, { plugins: [decompressTarxz()] }))
+        .then(_ => cache.cacheDir(zig_folder))
+        .then(_ => actions.addPath(zig_folder))
+        .catch(err => actions.error(err));
 }
 
 run();
